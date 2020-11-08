d8815234b4e35c6a36fd201f2f032801f3b2515c
Jacob Beers <jacobtbeers@gmail.com>
2020-11-08 00:54:18 -0600

    Add some css variables to begin theme support

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,27 +10,13 @@ const IndexPage = () => (
     <Layout>
         <SEO title="About" />
         <div className={style.content}>
-            {/* <div className={style.profilePic}>
-                <MeImage />
-            </div>
-            <h1 className={style.greeting}>Hi! My Name's Jacob!</h1>
-            <p className={style.intro}>
-                I'm a software developer based in good ol' KCMO
-            </p>
-            <p>
-                Professionally, I'm passionate about building awesome, durable software and learning from the trailblazers that have gone before me.
-                I love to collaborate and to do everything with as much excellence as I possibly can.
-           </p>
-            <p>
-                Personally, I love Jesus, my awesome family, riding my bike, and doodling.
-           </p> */}
             <div className = { style.top }>
                 <h1 className = { style.greeting }>Hi! My Name's Jacob!</h1>
                 <div className={style.profilePic}>
                     <MeImage  />
                 </div>
                 <p className={style.intro}>
-                    I'm a software developer based in good ol' KCMO 
+                    I'm a software developer based in good ol' KCMO.
                 </p>
             </div>
            <p>
